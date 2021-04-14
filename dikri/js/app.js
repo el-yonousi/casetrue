904f8bd36c54ad1d558144c2f7b152a4f0e6b231
el-yonousi <elyonousimohamed@gmail.com>
2021-04-14 08:55:04 +0000

    updates

--- dikri/js/app.js
+++ dikri/js/app.js
@@ -1,3 +1,14 @@
+if ("serviceWorker" in navigator)
+{
+    window.addEventListener("load", function ()
+    {
+        navigator.serviceWorker
+            .register("serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
+
 /* this function calc number repeat*/
 function dikrCount(card)
 {
